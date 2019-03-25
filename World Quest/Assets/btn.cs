e4df8eeb835ccc44bd302aab60d822979c624dad
10162 <10162@formacao.itap-net.org>
2019-03-25 15:15:05 +0000

    Added World Quest

---
+++ World Quest/Assets/btn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class btn : MonoBehaviour
+{
+
+  
+    public float delay = 440;
+    
+  
+     public void OnClick ()
+     {
+         StartCoroutine(LoadLevelAfterDelay(delay));
+        
+     }
+ 
+     IEnumerator LoadLevelAfterDelay(float delay)
+     {
+         yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+     }
+ 
+       
+
+}
