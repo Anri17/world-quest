eab57a8a49ce11c254492254d4d5079e4e47fad7
marquesdea <marquesdea@coventry.ac.uk>
2020-12-30 20:35:38 +0000

    Update Unity to 2020.1.9f1

---
+++ WorldQuest/Assets/GameOverData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverData : MonoBehaviour
+{
+    Text scoreText;
+    Text rightText;
+    Text wrongText;
+
+    void Start()
+    {
+        SetDependencies();
+        SetScore();
+    }
+
+    void SetDependencies()
+    {
+        scoreText = GameObject.Find("ScoreCount").GetComponent<Text>();
+        rightText = GameObject.Find("RightCount").GetComponent<Text>();
+        wrongText = GameObject.Find("WrongCount").GetComponent<Text>();
+    }
+
+    void SetScore()
+    {
+        scoreText.text = Data.scoreCount.ToString();
+        rightText.text = Data.rightAnswerCount.ToString();
+        wrongText.text = Data.wrongAnswerCount.ToString();
+    }
+}
