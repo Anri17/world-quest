2bf3fb28e94f0db9158cc176d38f1ff663ec007b
Anri17 <andralmeida1999@gmail.com>
2020-12-30 22:46:59 +0000

    Add default prefabs for QuizManager, SceneLoader and Camera

--- WorldQuest/Assets/Scripts/Core/QuizManager.cs
+++ WorldQuest/Assets/Scripts/Core/QuizManager.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-namespace WorldQuest
+namespace WorldQuest.Core
 {
     public class QuizManager : MonoBehaviour
     {
---
+++ WorldQuest/Assets/Scripts/Core/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace WorldQuest.Core
+{
+    public enum SceneIndex
+    {
+        TITLE,
+        EDITOR,
+        QUIZ
+    }
+
+    public class SceneLoader : MonoBehaviour
+    {
+        public void LoadScene(SceneIndex sceneIndex)
+        {
+            SceneManager.LoadScene((int)sceneIndex);
+        }
+    }
+}
